5b86709f6c369fa7bfa7005a5d72f659ded88fe8
ruchi13041995 <66416379+ruchi13041995@users.noreply.github.com>
2026-05-26 16:48:35 +0530

    Update main.tf
    changed image id

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-091138d0f0d41ff90" # replace this
   instance_type_value = "t2.micro"
   subnet_id_value = "subnet-019ea91ed9b5252e7" # replace this
 }
