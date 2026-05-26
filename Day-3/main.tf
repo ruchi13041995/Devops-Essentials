ce7322924ed5c48196e4842f7c4cdf65306c736b
ruchi13041995 <66416379+ruchi13041995@users.noreply.github.com>
2026-05-26 16:49:48 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-091138d0f0d41ff90" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7" # replace this
+ # subnet_id_value = "subnet-019ea91ed9b5252e7" # replace this
 }
